a3664ad7bd69f04ed4ecdb511491ef0939af2581
Abhay999-web <singhabhaypratap609@gmail.com>
2026-05-17 12:28:01 +0530

    updated origin link for live frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors({
-    origin:  "http://localhost:5173",
+    origin:  "https://travel-bharat-in-1.onrender.com",
     credentials: true
 }))
 
